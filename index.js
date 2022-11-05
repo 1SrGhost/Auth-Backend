bf99a921ef3ead81bed1b1afece68e272edf29c3
Andres Perez <edgar4698@hotmail.com>
2022-11-05 04:12:22 -0500

    conexion mongodb

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors  = require('cors');
+const { dbConnection } = require('./db/config.js');
 require('dotenv').config();
 
 console.log(process.env)
 // crear el servidor/ aplicacion de express
 const app = express();
+//Base de datos
+dbConnection();
+
 //Directorio Publico
 
 app.use(express.static('public'))
