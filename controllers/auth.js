acfab68ccc1c623c2c4517118a77414fd99a85db
Andres Perez <edgar4698@hotmail.com>
2022-11-06 03:28:55 -0500

    Crear y validar nuevo usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,51 @@
 const { response } = require('express');
+const Usuario = require('../models/Usuario');
 
 
 
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async (req, res = response) => {
 
 
 
     const { email, name, password } = req.body;
-    console.log(email, name, password)
-    return res.json({
+
+    try { 
+    
+    //Verificar el email
+    let usuario = await Usuario.findOne({email:email});
+    //Validar que el usuario no este creado
+    if ( usuario) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'El usuario con ese email ya existe'
+        });
+    }
+    //Crear Usuario con el modelo
+    const dbUser = new Usuario(req.body);
+    //Hash de la contrasena 
+
+    //Generar el JWT
+    //Crear Usuario de DB
+    await dbUser.save();
+    //Generar respuesta exitosa
+    return res.status(201).json({
         ok: true,
-        msg: 'crear usuario /new'
+        uid: dbUser.id,
+        name,
     })
+    }
+    catch (error) {
+        return res.status(500).json({
+            ok: false,
+            msg: 'por favor hable con el administrador'
+        })
+
+    }
+
+
+
+
 }
 
 const loginUsuario = (req, res = response) => {
